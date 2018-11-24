4c6a24a2b70f25d7e6e857d4a06f7e15fef41c0f
calvemoe <thephonep@gmail.com>
2018-11-24 21:11:02 +0200

    Pong test commit after .gitignore was added

---
+++ Assets/Project/Scripts/Ball.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    //increase ball speed after bouncing the paddle
+    public float speedMultiplier = 1.33f;
+
+    //speeds for h Axis
+    public float minXSpeed = 0.8f; 
+    public float maxXSpeed = 1.2f;
+
+    //speeds for v Axis
+    public float minYSpeed = 0.8f; 
+    public float maxYSpeed = 1.2f; 
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+        //apply speed to ball
+        rb.velocity = new Vector2(
+            Random.Range(minXSpeed, maxXSpeed) * Random.value > 0.5f ? -1 : 1, 
+            Random.Range(minYSpeed, maxYSpeed) * Random.value > 0.5f ? -1 : 1
+        );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //bouncing ball
+        if(other.CompareTag("Limit"))
+        {
+            GetComponent<AudioSource>().Play();
+
+            //top gamefield limit
+            if (other.transform.position.y > transform.position.y && rb.velocity.y > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+            }
+            //bottom gamefield limit
+            if (other.transform.position.y < transform.position.y && rb.velocity.y < 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+            }
+        }
+        else if (other.CompareTag("Paddle"))
+        {
+            GetComponent<AudioSource>().Play();
+
+            //left paddle
+            if (other.transform.position.x < transform.position.x && rb.velocity.x < 0)
+            {
+                rb.velocity = new Vector2(
+                    -rb.velocity.x * speedMultiplier, 
+                    rb.velocity.y * speedMultiplier
+                );
+            }
+            //right paddle
+            if (other.transform.position.x > transform.position.x && rb.velocity.x > 0)
+            {
+                rb.velocity = new Vector2(
+                    -rb.velocity.x * speedMultiplier, 
+                    rb.velocity.y * speedMultiplier
+                );
+            }
+        }
+
+    }
+}
---
+++ Assets/Project/Scripts/GameContoller.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameContoller : MonoBehaviour {
+
+    //Score labels
+    public Text Score1Text;
+    public Text Score2Text;
+
+    //coordinates out of the screen
+    public float scoreCoordianates = 3.3f;
+
+    //for ball instance
+    public GameObject ballPrefab;
+
+    private Ball currentBall;
+
+    //Score values
+    private int score1 = 0;
+    private int score2 = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        SpawnBall();
+	}
+
+    void SpawnBall()
+    {
+        //create ball in game scene
+        GameObject ballInstance = Instantiate(ballPrefab, transform);
+
+        currentBall = ballInstance.GetComponent<Ball>();
+        currentBall.transform.position = Vector3.zero;
+
+        //initiate score at game start
+        Score1Text.text = score1.ToString();
+        Score2Text.text = score2.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentBall != null)
+        {
+            if (currentBall.transform.position.x > scoreCoordianates)
+            {
+                //scoring for player 1
+                score1++;
+                //destroy ball after scoring
+                Destroy(currentBall.gameObject);
+                //create new ball on game scene
+                SpawnBall();
+            }
+
+            if (currentBall.transform.position.x < -scoreCoordianates)
+            {
+                //scoring for player 2
+                score2++;
+                //destroy ball after scoring
+                Destroy(currentBall.gameObject);
+                //create new ball on game scene
+                SpawnBall();                
+            }
+        }
+	}
+}
