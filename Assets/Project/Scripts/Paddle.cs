ad56457440c7c131b4916040d386d130c617ba6d
calvemoe <thephonep@gmail.com>
2019-05-29 06:12:24 +0300

    refactoring for optimization

--- Assets/Project/Scripts/Paddle.cs
+++ Assets/Project/Scripts/Paddle.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
-    public float speed = 1f;
+    [SerializeField]
+    private float speed = 1f;
 
     //player index for control
-    public int playerIndex = 1;
+    [SerializeField]
+    private int playerIndex = 1;
 
-
+    private Rigidbody2D rb;
     // Use this for initialization
     void Start () {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update () {
 
         float vAxis = Input.GetAxis("Vertical" + playerIndex);
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, vAxis * speed);
+        rb.velocity = new Vector2(0, vAxis * speed);
 
     }
 }
