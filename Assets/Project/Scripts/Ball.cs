ffba9eca4a7821920f4240df58e7ddad6733fa9a
calvemoe <thephonep@gmail.com>
2019-05-29 06:14:46 +0300

    refactoring for optimization used static ball instead of prefab, reworked logic for this

--- Assets/Project/Scripts/Ball.cs
+++ Assets/Project/Scripts/Ball.cs
@@ -5,77 +5,80 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
     //increase ball speed after bouncing the paddle
-    public float speedMultiplier = 1.33f;
+    [SerializeField]
+    private float speedMultiplier = 1.33f;
 
     //speeds for h Axis
-    public float minXSpeed = 0.8f; 
-    public float maxXSpeed = 1.2f;
+    [SerializeField]
+    private float minXSpeed = 0.8f; 
+    [SerializeField]
+    private float maxXSpeed = 1.2f;
 
     //speeds for v Axis
-    public float minYSpeed = 0.8f; 
-    public float maxYSpeed = 1.2f; 
+    [SerializeField]
+    private float minYSpeed = 0.8f; 
+    [SerializeField]
+    private float maxYSpeed = 1.2f; 
 
     private Rigidbody2D rb;
+    private AudioSource aus;
+    private Transform tr;
 
     //move direction
     private static int left = -1;
     private static int right = 1;
 
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        aus = GetComponent<AudioSource>();
+        tr = transform;
+    }
+
     // Use this for initialization
     void Start () {
-        rb = GetComponent<Rigidbody2D>();
+        this.ResetBall();
+	}
 
-        //randomize applying speed to ball in two directions
+    void OnTriggerEnter2D(Collider2D other) {
+        //bouncing ball
+        if(other.CompareTag("Limit")) {
+            aus.Play();
+
+            //top gamefield limit
+            if (other.transform.position.y > transform.position.y && rb.velocity.y > 0)
+                rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+
+            //bottom gamefield limit
+            if (other.transform.position.y < transform.position.y && rb.velocity.y < 0)
+                rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+        }
+        else if (other.CompareTag("Paddle")) {
+            aus.Play();
+
+            //left paddle
+            if (other.transform.position.x < transform.position.x && rb.velocity.x < 0) 
+                rb.velocity = new Vector2(
+                    -rb.velocity.x * speedMultiplier, 
+                    rb.velocity.y * speedMultiplier
+                );
+
+            //right paddle
+            if (other.transform.position.x > transform.position.x && rb.velocity.x > 0)
+                rb.velocity = new Vector2(
+                    -rb.velocity.x * speedMultiplier, 
+                    rb.velocity.y * speedMultiplier
+                );
+        }
+    }
+
+    public void ResetBall() {
+        //set ball position to center
+        tr.position = Vector3.zero;
+
+        //randomize applying speed to ball in four directions
         rb.velocity = new Vector2(
             Random.Range(minXSpeed, maxXSpeed) * Random.value > 0.5f ? left : right, 
             Random.Range(minYSpeed, maxYSpeed) * Random.value > 0.5f ? left : right
         );
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        //bouncing ball
-        if(other.CompareTag("Limit"))
-        {
-            GetComponent<AudioSource>().Play();
-
-            //top gamefield limit
-            if (other.transform.position.y > transform.position.y && rb.velocity.y > 0)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
-            }
-            //bottom gamefield limit
-            if (other.transform.position.y < transform.position.y && rb.velocity.y < 0)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
-            }
-        }
-        else if (other.CompareTag("Paddle"))
-        {
-            GetComponent<AudioSource>().Play();
-
-            //left paddle
-            if (other.transform.position.x < transform.position.x && rb.velocity.x < 0)
-            {
-                rb.velocity = new Vector2(
-                    -rb.velocity.x * speedMultiplier, 
-                    rb.velocity.y * speedMultiplier
-                );
-            }
-            //right paddle
-            if (other.transform.position.x > transform.position.x && rb.velocity.x > 0)
-            {
-                rb.velocity = new Vector2(
-                    -rb.velocity.x * speedMultiplier, 
-                    rb.velocity.y * speedMultiplier
-                );
-            }
-        }
-
     }
 }
--- Assets/Project/Scripts/GameContoller.cs
+++ Assets/Project/Scripts/GameContoller.cs
@@ -6,16 +6,16 @@ using UnityEngine.UI;
 public class GameContoller : MonoBehaviour {
 
     //Score labels
-    public Text Score1Text;
-    public Text Score2Text;
+    [SerializeField]
+    private Text Score1Text;
+    [SerializeField]
+    private Text Score2Text;
 
     //coordinates out of the screen
     public float scoreCoordianates = 3.3f;
 
-    //for ball instance
-    public GameObject ballPrefab;
-
-    private Ball currentBall;
+    [SerializeField]
+    private Ball ball;
 
     //Score values
     private int score1 = 0;
@@ -26,52 +26,40 @@ public class GameContoller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-        SpawnBall();
-	}
-
-    void SpawnBall()
-    {
-        //create ball in game scene
-        GameObject ballInstance = Instantiate(ballPrefab, transform);
-
-        currentBall = ballInstance.GetComponent<Ball>();
-        currentBall.transform.position = Vector3.zero;
-
         //initiate score at game start
         Score1Text.text = score1.ToString();
         Score2Text.text = score2.ToString();
-    }
+        
+        //throw the ball
+        ball.ResetBall();
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (currentBall != null)
-        {
-            if (currentBall.transform.position.x > scoreCoordianates)
-            {
-                //scoring for player 1
-                score1++;
-                if (score1 > MAXSCORE)
-                    score1 = 0;
-                
-                //destroy ball after scoring
-                Destroy(currentBall.gameObject);
-                //create new ball on game scene
-                SpawnBall();
-            }
+        if (ball.transform.position.x > scoreCoordianates) {
+            //scoring for player 1
+            score1++;
+            if (score1 > MAXSCORE)
+                score1 = 0;
 
-            if (currentBall.transform.position.x < -scoreCoordianates)
-            {
-                //scoring for player 2
-                score2++;
-                if (score2 > MAXSCORE)
-                    score2 = 0;
+            //put actual score on UI
+            Score1Text.text = score1.ToString();
 
-                //destroy ball after scoring
-                Destroy(currentBall.gameObject);
-                //create new ball on game scene
-                SpawnBall();                
-            }
+            //reset ball on game scene
+            ball.ResetBall();
+        }
+
+        if (ball.transform.position.x < -scoreCoordianates) {
+            //scoring for player 2
+            score2++;
+            if (score2 > MAXSCORE)
+                score2 = 0;
+
+            //put actual score on UI
+            Score2Text.text = score2.ToString();
+
+            //reset ball on game scene
+            ball.ResetBall();
         }
 	}
 }
